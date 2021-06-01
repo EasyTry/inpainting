20ae13425167d4e3295f54ca0203217234ea8084
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2021-06-01 18:38:16 +0300

    Update function.py

--- function.py
+++ function.py
@@ -7,5 +7,5 @@ model = Inpainter(height=1000)
 def call(img_n_mask):
 	inpainted = model(img_n_mask)
 	bytes_io = io.BytesIO()
-    	inpainted.save(bytes_io, format='JPEG')
-    	return bytes_io.getvalue()
+	inpainted.save(bytes_io, format='JPEG')
+	return bytes_io.getvalue()
