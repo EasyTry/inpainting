428efaa42aa306cc77adcbaca3d387ef82769729
Lucy Airapetyan <52498629+Leggerla@users.noreply.github.com>
2021-06-01 17:18:52 +0300

    Update function.py

--- function.py
+++ function.py
@@ -4,8 +4,8 @@ from model import Inpainter
 
 model = Inpainter(height=1000)
 
-def call(img, mask):
-	inpainted = model(img, mask)
+def call(img_n_mask):
+	inpainted = model(img_n_mask)
 	bytes_io = io.BytesIO()
     	inpainted.save(bytes_io, format='JPEG')
     	return bytes_io.getvalue()
